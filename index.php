e05b5440d1d3929dae4dda677789de45ee96a36f
tsukuyomi-07 <sebas.07d@gmail.com>
2024-11-05 21:01:00 -0500

    base de datos

--- index.php
+++ index.php
@@ -7,6 +7,54 @@
 </head>
 <body>
 
-    <h1>hola</h1>
+<form action="" method="get">
+<select name="desplegable">
+    <option value="1">opcion 1</option>
+    <option value="2">opcion 2</option>
+
+</select>
+<br> <br>
+<button type="submit">enviar</button>
+</form>
+
+<table border="1">
+    <thead>
+        <th>documentos</th>
+        <th>nombres</th>
+        <th>telefono</th>
+    </thead>
+
+    <tbody>
+        <tr>
+            <td>1</td>
+            <td>2</td>
+            <td>3</td>  
+        </tr>
+        <tr>
+            <td>4</td>
+            <td>5</td>
+            <td>6</td>  
+        </tr>
+       
+      <?php
+
+      $inicio = 1;
+      while($inicio < 10){
+        $inicio++;
+
+      ?>  
+        <tr>
+            <td>4</td>
+            <td>5</td>
+            <td>6</td>  
+        </tr>
+        
+      <?php  
+      }
+      ?>
+
+    </tbody>
+</table>
+
 </body>
 </html>
